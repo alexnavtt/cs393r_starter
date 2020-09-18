b67a900f28928f89fb9f637c43f271dd4d02d4b9
alexnavtt <alexnavtt@utexas.edu>
2020-09-18 14:49:17 -0500

    changed max curvature

--- src/navigation/navigation.cc
+++ src/navigation/navigation.cc
@@ -64,14 +64,14 @@ const float observation_delay_ = 0.0;
 const float actuation_delay_ = 0.0;
 const float vision_angle_ = 3*M_PI/2;  
 const float vision_range_ = 10; // based on sim, grid squares are 2m
-const float curvature_max_ = 1/1.0;
+const float curvature_max_ = 1/3.0;
 const float max_clearance_ = 2.0;
 
 // Fake Robot Parameters
 const float w = 0.25;	//width
 const float l = 0.4;	//length
 const float m = 0.1;	//padding
-const float b = 0.3;	//wheelbase
+const float b = 0.324;	//wheelbase
 const Vector2f pmin(0, w/2+m);
 const Vector2f pdif((b+l)/2+m, w/2+m);
 const Vector2f pmax((b+l)/2+m, -w/2-m);
