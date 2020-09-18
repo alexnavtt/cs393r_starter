07b17b12a41faa426cd547a7e0115f824cadfcce
MarkJennings <markjennings97@gmail.com>
2020-09-18 17:53:20 -0500

    added padding to clearance cost

--- src/navigation/navigation.cc
+++ src/navigation/navigation.cc
@@ -447,7 +447,7 @@ PathOption Navigation::getGreedyPath(Vector2f goal_loc)
 		float cost = - ( path.free_path_length / max_free_path_length ) * free_path_length_weight_		// (-) decrease cost with large FPL
 					 - ( path.clearance / max_clearance )		  		* clearance_weight_				// (+) increase cost with small clearance
 					 + ( path.distance_to_goal / min_distance_to_goal ) * distance_to_goal_weight_; 	// (+) increase cost with large distance to goal
-		// if ( path.clearance < (w/2+m*2) ) cost = cost*5; // significantly increase cost if not enough clearance (double padding on that calc)
+		if ( path.clearance < m ) cost = cost*5; // significantly increase cost if not enough clearance
 		// std::cout << "curvature: " << path.curvature << ", cost: " << cost << std::endl;
 
 		if (cost < min_cost) {min_cost = cost; BestPath = path;}
