f7fbe83dc373f8f8e59a75171b2e2b2c125f65f1
amrl-robot <amrl@utlists.utexas.edu>
2020-11-20 15:25:36 -0600

    slight modification to stall check

--- src/navigation/navigation.cc
+++ src/navigation/navigation.cc
@@ -304,6 +304,8 @@ void Navigation::checkStalled(){
 	if (not stalled_ and drive_msg_.velocity < 0.01){
 		stalled_ = true;
 		stall_time_ = ros::Time::now();
+	}else if(drive_msg_.velocity > 0.01){
+		stalled_ = false;
 	}
 }
 
@@ -352,6 +354,7 @@ void Navigation::Run() {
 		moveAlongPath(BestPath);
 		checkReached();
 
+		checkStalled();
 		if (global_planner_.needsReplan() or isRobotStuck()){
 			global_planner_.replan();
 			cout << "Replan!" << endl;
